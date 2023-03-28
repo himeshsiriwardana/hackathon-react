eef61a49834fe2400bba84b36a7a0c16af516b8e
himeshsiriwardana <himesh.siriwardana@gmail.com>
2023-03-28 18:29:24 +0530

    added inc, dec capabilities

--- src/global/CartReducer.js
+++ src/global/CartReducer.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 toast.configure();
 
 export const CartReducer = (state, action) => {
+
     const { shoppingCart, totalPrice, totalQty } = state;
 
     let product;
@@ -11,11 +12,12 @@ export const CartReducer = (state, action) => {
     let updatedPrice;
     let updatedQty;
 
-    switch(action.type) {
+    switch (action.type) {
 
-        case 'ADD_TO_CART' : 
+        case 'ADD_TO_CART':
+
             const check = shoppingCart.find(product => product.ProductID === action.id);
-            if(check){
+            if (check) {
                 toast.info('this product is already in your cart', {
                     position: "top-right",
                     autoClose: 2000,
@@ -25,11 +27,9 @@ export const CartReducer = (state, action) => {
                     draggable: false,
                     progress: undefined,
                 });
-                console.log("Product is already in your cart");
                 return state;
             }
-
-            else{
+            else {
                 product = action.product;
                 product['qty'] = 1;
                 product['TotalProductPrice'] = product.ProductPrice * product.qty;
@@ -41,7 +41,55 @@ export const CartReducer = (state, action) => {
             }
             break;
 
-            
-            
+        case 'INC':
+            product = action.cart;
+            product.qty = ++product.qty;
+            product.TotalProductPrice = product.qty * product.ProductPrice;
+            updatedQty = totalQty + 1;
+            updatedPrice = totalPrice + product.ProductPrice;
+            index = shoppingCart.findIndex(cart => cart.ProductID === action.id);
+            shoppingCart[index] = product;
+            return {
+                shoppingCart: [...shoppingCart], totalPrice: updatedPrice, totalQty: updatedQty
+            }
+            break;
+
+        case 'DEC':
+            product = action.cart;
+            if (product.qty > 1) {
+                product.qty = product.qty - 1;
+                product.TotalProductPrice = product.qty * product.ProductPrice;
+                updatedPrice = totalPrice - product.ProductPrice;
+                updatedQty = totalQty - 1;
+                index = shoppingCart.findIndex(cart => cart.ProductID === action.id);
+                shoppingCart[index] = product;
+                return {
+                    shoppingCart: [...shoppingCart], totalPrice: updatedPrice, totalQty: updatedQty
+                }
+            }
+            else {
+                return state;
+            }
+            break;
+
+        case 'DELETE':
+            const filtered = shoppingCart.filter(product => product.ProductID !== action.id);
+            product = action.cart;
+            updatedQty = totalQty - product.qty;
+            updatedPrice = totalPrice - product.qty * product.ProductPrice;
+            return {
+                shoppingCart: [...filtered], totalPrice: updatedPrice, totalQty: updatedQty
+            }
+            break;
+
+        case 'EMPTY':
+            return {
+                shoppingCart: [], totalPrice: 0, totalQty: 0
+            }
+
+        default:
+            return state;
+
     }
+
 }
